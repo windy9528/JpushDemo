09e52e0355e07b67f488ef72d5bcaea0027d5292
windy <a952814576@163.com>
2019-06-25 20:29:53 +0800

    Initial commit

---
+++ app/src/main/java/bwie/com/jpushdemo/MyService.java
@@ -0,0 +1,12 @@
+package bwie.com.jpushdemo;
+
+import cn.jpush.android.service.JCommonService;
+
+/**
+ * Author: 杨高峰(windy)
+ * Date: 2019/5/17 18:27
+ * Description:
+ */
+public class MyService extends JCommonService {
+
+}
